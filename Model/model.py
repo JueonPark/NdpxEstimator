ba524eaf6bcd5a8e4b98e1bd2e361ba244679ca9
jueonpark <jueonpark@postech.ac.kr>
2022-11-14 20:58:35 +0900

    more loss function experiments

--- Model/model.py
+++ Model/model.py
@@ -25,19 +25,17 @@ def build_and_compile_model(norm):
     norm,
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.BatchNormalization(),
-    # tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.BatchNormalization(),
-    # tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.BatchNormalization(),
     tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(128, activation='relu'),
-    # tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(1)
   ])
 
   # loss_fn = tf.keras.losses.BinaryCrossentropy()
+  # loss_fn = tf.keras.losses.MeanSquaredError
   loss_fn = tf.keras.losses.MeanAbsoluteError()
   model.compile(loss=loss_fn,
                 optimizer=tf.keras.optimizers.Adam(0.001))
